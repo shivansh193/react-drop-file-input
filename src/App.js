371e48c9f4eae7531a4289d17adf6d506ae66cfe
shivansh193 <shivanshkalra796@gmail.com>
2023-03-12 00:55:14 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import DropFileInput from './components/drop-file-input/DropFileInput';
-
+import FormFIelds from "./components/FormFields"
 function App() {
 
     const onFileChange = (files) => {
@@ -9,10 +9,12 @@ function App() {
     }
 
     return (
-        <div className="box">
+        <div className="box" style={{display: 'flex'
+        }}> 
             <h2 className="header">
                 React drop files input
             </h2>
+            <FormFIelds />
             <DropFileInput
                 onFileChange={(files) => onFileChange(files)}
             />
---
+++ src/components/FormFields.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function BasicTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Field1" variant="outlined" />
+      <TextField id="outlined-basic" label="Field2" variant="outlined" />
+      <TextField id="outlined-basic" label="Field3" variant="outlined" />
+      <TextField id="outlined-basic" label="Field4" variant="outlined" />
+      
+    </Box>
+  );
+}
